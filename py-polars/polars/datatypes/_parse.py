8d4c66ee5f772bce008a35292a95beb363b24d19
alexander-beedie <alexander.m.beedie@icloud.com>
2024-04-09 13:34:42 +0400

    feat(python): Improve dtype inference for `read_database` via driver type-code introspection

--- py-polars/polars/datatypes/_parse.py
+++ py-polars/polars/datatypes/_parse.py
@@ -31,7 +31,6 @@ from polars.datatypes.convert import is_polars_dtype
 if TYPE_CHECKING:
     from polars._typing import PolarsDataType, PythonDataType, SchemaDict
 
-
 UnionTypeOld = type(Union[int, str])
 if sys.version_info >= (3, 10):
     from types import NoneType, UnionType
@@ -79,6 +78,7 @@ def parse_py_type_into_dtype(input: PythonDataType | type[object]) -> PolarsData
         return String()
     elif input is bool:
         return Boolean()
+    # note: `datetime` is a subclass of `date`, so need to check `datetime` first
     elif isinstance(input, type) and issubclass(input, datetime):  # type: ignore[redundant-expr]
         return Datetime("us")
     elif isinstance(input, type) and issubclass(input, date):  # type: ignore[redundant-expr]
